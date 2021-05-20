2aae9aba2621e6053251c8dd6a60176113990881
roryfrench7 <roryfrench7@gmail.com>
2021-05-20 11:31:39 -0700

    initial commit

---
+++ UTM_to_DecimalDegrees.R
@@ -0,0 +1,21 @@
+library(sp)
+
+setwd("D:/Kobold_Metals/UTM_Coord")
+
+coord_set = list.files(getwd(), pattern = ".csv")
+print(coord_set)
+i=1
+coord_list = read.csv(coord_set[i])
+
+utm_coord <- SpatialPoints(cbind(coord_list[1],coord_list[2]), proj4string=CRS("+init=epsg:26716"))
+
+wgs_coord <- spTransform(utm_coord, CRS("+proj=longlat"))
+
+write.csv(wgs_coord, coord_set[i])
+
+
+
+
++init=epsg:26718 "NAD27 Zone 18N"
++init=epsg:26716 "NAD27 Zone 16N"
++proj=utm +zone=15N "NAD84 Zone 15N"
