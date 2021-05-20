02f162f6390452f017a34242d6bbe92d8b2757cc
roryfrench7 <roryfrench7@gmail.com>
2021-05-20 11:32:35 -0700

    second commit

--- UTM_to_DecimalDegrees.R
+++ UTM_to_DecimalDegrees.R
@@ -1,11 +1,10 @@
 library(sp)
 
-setwd("D:/Kobold_Metals/UTM_Coord")
 
-coord_set = list.files(getwd(), pattern = ".csv")
+coord_set <- list.files("D:/Kobold_Metals/UTM_Coord", pattern = ".csv")
 print(coord_set)
 i=1
-coord_list = read.csv(coord_set[i])
+coord_list <- read.csv(coord_set[i])
 
 utm_coord <- SpatialPoints(cbind(coord_list[1],coord_list[2]), proj4string=CRS("+init=epsg:26716"))
 
