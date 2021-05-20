264f27a444f71e8b0c8b724ea28d1ec01e4816e9
roryfrench7 <roryfrench7@gmail.com>
2021-05-20 11:37:40 -0700

    initial commit

---
+++ Reclassify_Raster.R
@@ -0,0 +1,23 @@
+library(sf)
+library(spbabel)
+library(raster)
+
+
+island_set <- list.files("D:/Coconut_Imagery/Resampled_MS", pattern = ".tif")
+island_list <- list()
+
+for (i in 1:length(island_set)) {
+  
+  old_tif <- stack(island_set[i])
+  
+  
+  new_tif <- reclassify(old_tif, cbind(-Inf, 0, NA))
+  
+  island_output <- paste0(substr(island_set[[i]], 1, nchar(island_set[[i]])-4), "_mosaic_resampled.tif")
+  writeRaster(new_tif, island_output, overwrite =TRUE)
+  
+  island_list[i] <- island_output
+  print(paste0("finished resampling count ", i, ": ",  island_set[i], " at ", Sys.time()))
+}
+print(island_list)
+
