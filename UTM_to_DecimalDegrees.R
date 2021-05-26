2ebe171cfcf9bc08b2c4cd16a66c222ffe41e66d
roryfrench7 <roryfrench7@gmail.com>
2021-05-25 23:29:01 -0700

    added comments

--- UTM_to_DecimalDegrees.R
+++ UTM_to_DecimalDegrees.R
@@ -1,16 +1,20 @@
-library(sp)
+"""
+Read data table and convert UTM xy coordinates to a specified coordinate system. 
+The first two columns of the input csv should be the XY coordinates.
+"""
 
+library(sp)
 
 coord_set <- list.files("D:/Kobold_Metals/UTM_Coord", pattern = ".csv")
 print(coord_set)
 i=1
 coord_list <- read.csv(coord_set[i])
 
-utm_coord <- SpatialPoints(cbind(coord_list[1],coord_list[2]), proj4string=CRS("+init=epsg:26716"))
+utm_coord <- SpatialPoints(cbind(coord_list[1],coord_list[2]), proj4string=CRS("+init=epsg:26716")) #Create shapefile from input csv, need to assigns its existing coord system
 
-wgs_coord <- spTransform(utm_coord, CRS("+proj=longlat"))
+wgs_coord <- spTransform(utm_coord, CRS("+proj=longlat")) #Reproject shapefile to specified coordinate system
 
-write.csv(wgs_coord, coord_set[i])
+write.csv(wgs_coord, coord_set[i]) #Rewrite new coordinate points to file
 
 
 
